aa66de53a67129b74bbeba2763c195e54aa549df
Ya-Ching <airatang0150@gmail.com>
2026-02-11 16:45:15 -0500

    temp

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import { renderLaunchMap } from "./launchmap.js";
+
+renderLaunchMap({
+  containerSelector: "#launchmap",
+  dropdownSelector: "#continent",
+  csvPath: "data/satcat_with_continent.csv",
+  worldTopoPath: "data/world-110m.json",
+  defaultContinent: "Asia",
+});
 
 // Declare the chart dimensions and margins.
 const width = 640;
@@ -10,29 +18,31 @@ const marginBottom = 30;
 const marginLeft = 40;
 
 // Declare the x (horizontal position) scale.
-const x = d3.scaleUtc()
-    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-    .range([marginLeft, width - marginRight]);
+const x = d3
+  .scaleUtc()
+  .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+  .range([marginLeft, width - marginRight]);
 
 // Declare the y (vertical position) scale.
-const y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([height - marginBottom, marginTop]);
+const y = d3
+  .scaleLinear()
+  .domain([0, 100])
+  .range([height - marginBottom, marginTop]);
 
 // Create the SVG container.
-const svg = d3.create("svg")
-    .attr("width", width)
-    .attr("height", height);
+const svg = d3.create("svg").attr("width", width).attr("height", height);
 
 // Add the x-axis.
-svg.append("g")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x));
+svg
+  .append("g")
+  .attr("transform", `translate(0,${height - marginBottom})`)
+  .call(d3.axisBottom(x));
 
 // Add the y-axis.
-svg.append("g")
-    .attr("transform", `translate(${marginLeft},0)`)
-    .call(d3.axisLeft(y));
+svg
+  .append("g")
+  .attr("transform", `translate(${marginLeft},0)`)
+  .call(d3.axisLeft(y));
 
 // Append the SVG element.
 container.append(svg.node());
